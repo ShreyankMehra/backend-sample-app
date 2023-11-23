3335bcd47f173f6a76a359559a48b949027321fe
ShreyankMehra <mehrashreyank12345@gmail.com>
2023-11-23 15:41:17 +0530

    index.js modified

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require('express')
 require("dotenv").config()
 const app = express()
 const port = 3000
-
+data1=    [{"menu": {  
+    "id": "file",  
+    "value": "File",  
+    "popup": {  
+      "menuitem": [  
+        {"value": "New", "onclick": "CreateDoc()"},  
+        {"value": "Open", "onclick": "OpenDoc()"},  
+        {"value": "Save", "onclick": "SaveDoc()"}  
+      ]  
+    }  
+  }} ] 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })                       
 app.get('/home', (req, res) => {
   res.send('<h1>This is the home page!!</h1>')
-})                       
+}) 
+app.get('/json', (req, res) => {
+  res.json(data1)
+}) 
+
+
   app.listen(process.env.PORT, () => {
-      console.log(`Example app listening on port ${port}`)
+      console.log(`Example app listening on port ${process.env.PORT}`)
 })
